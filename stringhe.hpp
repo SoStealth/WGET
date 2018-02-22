0e753988a95104a9cf1dc92c2a0bc2c00d14b25c
SoStealth <36263330+SoStealth@users.noreply.github.com>
2018-02-22 11:50:02 +0000

    Update stringhe.hpp

--- stringhe.hpp
+++ stringhe.hpp
@@ -1,12 +1,6 @@
 /*
 stringhe.h
 */
-void* duplica(char* buffer, int* length) {
-	char* ret = (char*)malloc(sizeof(char*)*(*length+1));
-	for(int i=0;i<=*length;i++)
-		*(ret+i)=*(buffer+i);
-	return ret;
-}
 char* concat(char* first_string,char* second_string) {
 	char* ret=(char*)malloc(strlen(first_string)+strlen(second_string)+1);
 	char* tmp = ret;
